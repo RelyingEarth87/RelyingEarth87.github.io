515cc22d4ff90a1eee4d029f2794c22e71842c5a
Adam <47026870+RelyingEarth87@users.noreply.github.com>
2023-04-26 22:44:01 -0400

    Update coding-journey.php

--- programming/coding-journey.php
+++ programming/coding-journey.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <?php>include("E:\code\githubsite\navbar.html");</?>
+    <?php>include("https://RelyingEarth87.github.io/navbar.html");</?>
     <div id="page">
     <div id="contents">
         <label>Table of Contents</label>
